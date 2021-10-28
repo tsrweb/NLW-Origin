2caa7cee57b819cf01afb0bc4b9abe082e7e287e
Tiago Rodrigues <tiagorodrigues@byteos.local>
2021-10-28 16:45:54 -0300

    Atualizando implementações em JavaScript

--- js/main.js
+++ js/main.js
@@ -13,4 +13,20 @@ function changeSocialMediaLinks() {
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userGitHub.textContent = data.login
+      userImage.src = data.avatar_url
+    })
+}
+
 changeSocialMediaLinks()
+getGitHubProfileInfos()
